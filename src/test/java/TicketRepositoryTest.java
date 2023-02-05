a5bbaae04069e50d086132176052286e1f91efb4
Ilya <godfrid7@mail.ru>
2023-02-05 22:44:49 +0300

    src

--- src/test/java/TicketRepositoryTest.java
+++ src/test/java/TicketRepositoryTest.java
@@ -82,4 +82,31 @@ class TicketRepositoryTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void testDeleteTicket() {
+        TicketRepository repo = new TicketRepository();
+
+        Ticket ticket1 = new Ticket(1, "MSK", "SPB", 200, 93);
+        Ticket ticket2 = new Ticket(2, "MSK", "SPB", 100, 98);
+        Ticket ticket3 = new Ticket(3, "MSK", "UFA", 200, 186);
+        Ticket ticket4 = new Ticket(4, "MSK", "SPB", 400, 122);
+        Ticket ticket5 = new Ticket(5, "UFA", "SPB", 800, 222);
+        Ticket ticket6 = new Ticket(6, "MSK", "SPB", 200, 130);
+        Ticket ticket7 = new Ticket(7, "NEW", "LA", 300, 243);
+        Ticket ticket8 = new Ticket(8, "MSK", "SPB", 500, 99);
+        repo.add(ticket1);
+        repo.add(ticket2);
+        repo.add(ticket3);
+        repo.add(ticket4);
+        repo.add(ticket5);
+        repo.add(ticket6);
+        repo.add(ticket7);
+        repo.add(ticket8);
+
+        Ticket[] expected = { ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8};
+        Ticket[] actual = repo.deleteById(1);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
